2e9c7b41b78174616bdeb190fc198862a9ddc036
grainerycafe <parksangwoo87@gmail.com>
2018-02-02 10:38:20 -0800

    test commit

--- app/src/main/java/ca/bcit/comp3717project/MainActivity.java
+++ app/src/main/java/ca/bcit/comp3717project/MainActivity.java
@@ -17,4 +17,6 @@ public class MainActivity extends AppCompatActivity {
         Intent goToMap = new Intent(this, MapActivity.class);
         startActivity(goToMap);
     }
+
+    //added comments
 }

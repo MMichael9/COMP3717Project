b86b09b3c4edefb84a44428975bacee29cb70664
grainerycafe <parksangwoo87@gmail.com>
2018-03-23 13:46:55 -0700

    venues data working

---
+++ app/src/main/java/ca/bcit/comp3717project/Culture.java
@@ -0,0 +1,12 @@
+package ca.bcit.comp3717project;
+
+/**
+ * Created by sangwoorai on 2018-03-23.
+ */
+
+public class Culture extends Amenity {
+    @Override
+    public int getValue(Preferences pref) {
+        return (int) (pref.culture * 0.1);
+    }
+}
--- app/src/main/java/ca/bcit/comp3717project/MainActivity.java
+++ app/src/main/java/ca/bcit/comp3717project/MainActivity.java
@@ -1,25 +1,15 @@
 package ca.bcit.comp3717project;
 
 import android.content.Intent;
-import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-
-import com.google.android.gms.maps.model.LatLng;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         downloadShoppingData();
         downloadSchoolData();
+        downloadCultureData();
     }
 
     public void toMap(final View view) {
@@ -119,18 +110,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /*public void downloadParkData() {
-        Log.d(TAG, "downloadSchoolData: DOWNLOADING");
+    public void downloadCultureData() {
+        Log.d(TAG, "downloadCultureData: DOWNLOADING");
         Ion.with(this).
-                load("http://opendata.newwestcity.ca/downloads/parks/PARKS.json").
-                asJsonArray().
+                load("http://opendata.newwestcity.ca/downloads/cultural-venues/VENUES.json").
+                asJsonObject().
                 setCallback(
-                        new FutureCallback<JsonArray>() {
+                        new FutureCallback<JsonObject>() {
                             @Override
                             public void onCompleted(final Exception ex,
-                                                    final JsonArray result) {
+                                                    final JsonObject result) {
                                 if (ex == null) {
-                                    downloadParkSuccess(result);
+                                    downloadCultureSuccess(result);
                                 } else {
                                     downloadError(ex);
                                 }
@@ -139,27 +130,26 @@ public class MainActivity extends AppCompatActivity {
                 );
     }
 
-    private void downloadParkSuccess(final JsonArray jsonArray) {
-        for (final JsonElement element : jsonArray) {
-            final JsonObject feature = element.getAsJsonObject();
-            final String name = feature.get("Name").getAsString();
-            final JsonArray polygonArray = feature.get("json_geometry").getAsJsonObject().get("coordinates").getAsJsonArray();
-            final ArrayList<LatLng> parkCoords = new ArrayList<>();
-            for (final JsonElement coords : polygonArray) {
-                JsonArray coord = coords.getAsJsonArray();
+    private void downloadCultureSuccess(final JsonObject jsonObject) {
+        JsonArray venuesArray = jsonObject.getAsJsonArray("features");
+
+        for (final JsonElement element : venuesArray) {
+            final JsonObject properties = element.getAsJsonObject().getAsJsonObject("properties");
+            if (!properties.get("Name").isJsonNull()) {
+                final String name = properties.get("Name").getAsString();
+                final double lat = properties.get("Y").getAsDouble();
+                final double lng = properties.get("X").getAsDouble();
+                final Amenity newAmenity = new Culture();
+
+                newAmenity.setName(name);
+                newAmenity.setLat(lat);
+                newAmenity.setLng(lng);
+
+                AMENITIES.add(newAmenity);
+                Log.d(TAG, "downloadSuccess: " + name);
             }
-            final double lat = feature.get("Y").getAsDouble();
-            final double lng = feature.get("X").getAsDouble();
-            final Amenity newAmenity = new Shopping();
-
-            newAmenity.setName(name);
-            newAmenity.setLat(lat);
-            newAmenity.setLng(lng);
-
-            AMENITIES.add(newAmenity);
-            Log.d(TAG, "downloadSuccess: " + name);
         }
-    }*/
+    }
 
     private static void downloadError(final Exception ex) {
         ex.printStackTrace();
